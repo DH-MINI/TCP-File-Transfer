f253fd65a906538aed74c0689e9756448eb6588d
DH-MINI <sunyiwei0401@gmail.com>
2024-06-02 13:44:35 +0800

    Argument length error handling

--- Client/src_client/client.c
+++ Client/src_client/client.c
@@ -25,8 +25,28 @@ int main()
         printf("\033[1;31mclient\033[0m (%s): ==> ", (mode == MODE_REMOTE) ? "\033[1;32m REMOTE \033[0m" : "\033[1;32m LOCAL  \033[0m");
         // 读取用户输入
         fgets(buffer, sizeof(buffer), stdin);
+
+        // 检查用户的输入是否超过了 buffer 的大小
+        if (buffer[strlen(buffer) - 1] != '\n')
+        {
+            // printf("Error: Input too long.\n");
+            print_client_message("ERROR", "Input too long.", "\033[1;31m");
+
+            // 清空输入缓冲区
+            int ch;
+            while ((ch = getchar()) != '\n' && ch != EOF)
+                ;
+            continue;
+        }
+
         buffer[strcspn(buffer, "\n")] = 0;
         int num_args = sscanf(buffer, "%s %s %s", command, arg1, arg2);
+        if (strlen(arg1) > 32 || strlen(arg2) > 32)
+        {
+            // printf("Error: Argument too long.\n");
+            print_client_message("ERROR", "Argument too long.", "\033[1;31m");
+            continue;
+        }
         // printf("num_args: %d\n", num_args);
         if (num_args == -1)
         {
